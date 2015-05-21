a00de6725992605445ece1072e9bcf26a88f1f90
joeharker <joeharker@gmail.com>
2015-05-20 20:08:16 -0600

    delivery request updates

--- Fetch1/demo/demoController.js
+++ Fetch1/demo/demoController.js
@@ -1,24 +1,22 @@
 ﻿/*global app */
 app.controller('DemoCtrl', ['DemoSrvc',
   function (DemoSrvc) {
-      'use strict';
+    'use strict';
   	var c = this;
 
   	c.pickup = DemoSrvc.get('pickup');
   	c.delivery = DemoSrvc.get('delivery');
-  	c.price = DemoSrvc.get('price');
+  	c.weight = DemoSrvc.get('weight');
+  	c.content = DemoSrvc.get('content');
+  	c.instructions = DemoSrvc.get('instructions');
+  	c.size = (DemoSrvc.get('size') === '') ? 'small' : DemoSrvc.get('size');
 
-  	c.setPickup = function () {
-  	    DemoSrvc.set('pickup', c.pickup);
-  	};
-
-  	c.setDelivery = function () {
-  	    DemoSrvc.set('delivery', c.delivery);
-  	};
-
-  	c.setPrice = function () {
-  	    DemoSrvc.set('price', c.price);
-  	};
+  	c.setPickup = function () { DemoSrvc.set('pickup', c.pickup); };
+  	c.setDelivery = function () { DemoSrvc.set('delivery', c.delivery); };
+  	c.setWeight = function () { DemoSrvc.set('weight', c.weight); };
+  	c.setContent = function () { DemoSrvc.set('content', c.content); };
+  	c.setInstructions = function () { DemoSrvc.set('instructions', c.instructions); };
+  	c.setSize = function (size) { DemoSrvc.set('size', size); };
 
   	return c;
   }]);
