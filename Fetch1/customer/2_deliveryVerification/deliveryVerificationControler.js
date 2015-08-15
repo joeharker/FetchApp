41fc4af3f41fba9169864e4a84894c60d60cfdcc
joeharker <joeharker@gmail.com>
2015-08-14 18:38:35 -0600

    hiding debug

--- Fetch1/customer/2_deliveryVerification/deliveryVerificationControler.js
+++ Fetch1/customer/2_deliveryVerification/deliveryVerificationControler.js
@@ -18,8 +18,8 @@ function (mapService, $q) {
 					break;
 				case 'medium':
 					price = 14.95;
-					price += 0.0021 * rout.meters;
-					price += 0.0083 * rout.seconds;
+					price += 0.0007 * rout.meters;
+					price += 0.0025 * rout.seconds;
 					price = price < 25 ? 25 : price;
 
 					break;
