6f1013d2c7c9996ed20079f5e91294841635f22b
joeharker <joeharker@gmail.com>
2015-08-16 12:28:12 -0600

    auth test

--- Fetch1/app/authentication/authenticateController.js
+++ Fetch1/app/authentication/authenticateController.js
@@ -6,6 +6,17 @@ app.controller('AuthCtrl', [
 
         c.clientId = 'ca_5oSXm50hso4vFBv5IvDHVVklCiUkdP8R';
         c.publishableKey = 'pk_test_LDonp15qE0gZigryIcfweU23';
+        c.win = {};
+
+        c.test = function () {
+        	c.win = window.open('https://connect.stripe.com/oauth/authorize?response_type=code&client_id=' + c.clientId);
+        	//c.win = window.open(document.location.href);
+        };
+
+        c.show = function () {
+        	console.log(c.win);
+        	console.log(c.win.closed);
+        };
 
         return c;
     }]);
