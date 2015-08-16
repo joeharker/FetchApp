7ac860a3700fa023c7c8668f55647a79c360df27
joeharker <joeharker@gmail.com>
2015-08-15 22:34:46 -0600

    working with stripe

--- Fetch1/customer/2.5_payment/paymentControler.js
+++ Fetch1/customer/2.5_payment/paymentControler.js
@@ -1,25 +1,29 @@
 ﻿/*global app */
-app.controller('PayCtrl', ['$q',
-function ($q) {
+app.controller('PayCtrl', ['$q', '$scope', '$http',
+function ($q, $scope, $http) {
 	var c = this;
 
 	//TODO: get key from service
 	Stripe.setPublishableKey('pk_test_LDonp15qE0gZigryIcfweU23');
 
 	c.submit = function (card) {
-		card.stripeToken = 'testing';
-		console.log(['card', card]);
+		//TODO: disable the submit
 
 		Stripe.card.createToken(card, function (status, response)
 		{
-			//TODO: disable the submit
-			console.log(['response', response]);
-			console.log(['status', status]);
-
 			if (status != 200) {
 				alert(response.message);
 			} else {
 				card.stripeToken = response.id;
+				$scope.$applyAsync();
+				var url = 'http://fetch001.azurewebsites.net/api/pay?cents=' + card.amount + '&token=' + card.stripeToken + '&desc=' + card.content;
+				console.log(url);
+				$http.get(url)
+				.then(function (r) {
+					console.log(r);
+				}, function (e) {
+					console.log(e);
+				});
 			}
 		});
 	};
