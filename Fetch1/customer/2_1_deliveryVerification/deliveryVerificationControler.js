f8f9a6c7c3fbe004bb96079bf4cf43f04eef9ba2
joeharker <joeharker@gmail.com>
2015-08-21 19:16:18 -0600

    payment to continue working

--- Fetch1/customer/2_1_deliveryVerification/deliveryVerificationControler.js
+++ Fetch1/customer/2_1_deliveryVerification/deliveryVerificationControler.js
@@ -1,6 +1,7 @@
-﻿/*global app */
-app.controller('DeliveryVerificationCtrl', ['mapService', '$q', 'DemoSrvc',
-function (mapService, $q, DemoSrvc) {
+﻿/// <reference path="../3_trackMap/trackMap.html" />
+/*global app */
+app.controller('DeliveryVerificationCtrl', ['mapService', '$q', 'DemoSrvc', '$interval',
+function (mapService, $q, DemoSrvc, $interval) {
 	var c = this;
 
 	c.calculateCost = function (form) {
@@ -39,6 +40,42 @@ function (mapService, $q, DemoSrvc) {
 		}, function (reason) {
 			console.log(['Failed', reason]);
 		});
+	};
+
+	//c.init = function (page) {
+	var thisToken = {};
+		var	handler = StripeCheckout.configure({
+				key: 'pk_test_LDonp15qE0gZigryIcfweU23',
+				image: '../../resources/icon/icon.png',
+				locale: 'auto',
+				token: function (token) {
+					// Use the token to create the charge with a server-side script.
+					// You can access the token ID with `token.id`
+					thisToken = token;
+				}
+			})
+		;
+	//};
+		var ticker;
+
+	c.submit = function (description, cents, page) {
+		// Open Checkout with further options
+		handler.open({
+			name: 'FETCH1 TRANSPORT LLC',
+			description: description,
+			zipCode: true,
+			amount: cents
+		});
+
+		ticker = $interval(function () {
+			if (thisToken.id !== undefined) {
+				$interval.cancel(ticker);
+				console.log(thisToken);
+				page.load('customer/3_trackMap/trackMap.html');
+			}
+		}, 1000);
+
+		//page.load();
 	};
 
 	return c;
