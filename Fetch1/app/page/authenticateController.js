60cdc47a756af00df28b1c8ef902085860588295
joeharker <AhtXtqgCbIKnJOstZwBN>
2015-10-01 18:26:25 -0600

    i bet this was it

--- Fetch1/app/page/authenticateController.js
+++ Fetch1/app/page/authenticateController.js
@@ -11,15 +11,10 @@ app.controller('AuthCtrl', ['GuidService', 'ConfigSrvc', 'MemorySrvc', '$interva
         c.newer = false;
         c.next = '';
 
-        MemorySrvc.reset();
-
         c.oauth = function (page) {
-            if (cordova !== null && cordova !== undefined && cordova.InAppBrowser !== undefined) {
-                window.open = cordova.InAppBrowser.open;
-            }
-            c.win = window.open('https://connect.stripe.com/oauth/authorize?response_type=code&scope=read_write&client_id=' + ConfigSrvc.stripeClientId + '&state=' + c.guid + '&stripe_user[business_name]=Neighborhood driver&stripe_user[business_type]=sole_prop&stripe_user[physical_product]=false&stripe_user[url]=http://www.fetch1.com/', '_system');
+        	c.win = window.open('https://connect.stripe.com/oauth/authorize?response_type=code&scope=read_write&client_id=' + ConfigSrvc.stripeClientId + '&state=' + c.guid + '&stripe_user[business_name]=Neighborhood driver&stripe_user[business_type]=sole_prop&stripe_user[physical_product]=false&stripe_user[url]=http://www.fetch1.com/', '_blank');
 
-            //wait for confirmation
+        	//wait for confirmation
         	ticker = $interval(function () {
         		$http.get(ConfigSrvc.serviceUrl + '/api/auth?guid=' + c.guid)
 					.then(function (response) {
@@ -27,10 +22,6 @@ app.controller('AuthCtrl', ['GuidService', 'ConfigSrvc', 'MemorySrvc', '$interva
 							$interval.cancel(ticker);
 							MemorySrvc.set('myId', response.data);
 							c.win.close();
-
-							if (cordova !== null && cordova !== undefined && cordova.InAppBrowser !== undefined) {
-							    delete window.open;
-							}
 							page.load('driver/1_pickupMap/pickupMap.html');
 						}
 					}, function (e) {
