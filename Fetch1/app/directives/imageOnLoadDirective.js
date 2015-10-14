0adf4ca86ad646c9e03ed82e600aa40baa589421
joeharker <AhtXtqgCbIKnJOstZwBN>
2015-10-13 20:16:58 -0600

    etst 2

---
+++ Fetch1/app/directives/imageOnLoadDirective.js
@@ -0,0 +1,10 @@
+﻿app.directive('imageonload', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            element.bind('load', function () {
+                alert("s="+ scope +" e="+ element +" a="+ attrs);
+            });
+        }
+    };
+});
