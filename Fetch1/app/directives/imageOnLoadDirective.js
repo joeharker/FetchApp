1ffbdef006e1b085c641a777bfac042a842359e2
joeharker <AhtXtqgCbIKnJOstZwBN>
2015-10-13 21:32:15 -0600

    element

--- Fetch1/app/directives/imageOnLoadDirective.js
+++ Fetch1/app/directives/imageOnLoadDirective.js
@@ -4,8 +4,8 @@
         link: function (scope, element, attrs) {
             element.bind('load', function () {
                 try {
-                    alert(JSON.stringify(attrs));
-                    scope.$eval(attrs.imageonloadFunction);
+                    alert(JSON.stringify(element));
+                    scope.$eval(element.imageonloadFunction);
                 } catch (e) {
                     alert(e.message);
                 }
