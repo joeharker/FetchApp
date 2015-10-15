39d57522f64615ee53853f90715de0126e48e940
joeharker <AhtXtqgCbIKnJOstZwBN>
2015-10-14 21:29:25 -0600

    wait first

--- Fetch1/app/page/authenticateController.js
+++ Fetch1/app/page/authenticateController.js
@@ -14,22 +14,23 @@ app.controller('AuthCtrl', ['GuidService', 'ConfigSrvc', 'MemorySrvc', '$interva
         MemorySrvc.reset();
 
         c.oauth = function (page) {
-            c.win = window.open('https://connect.stripe.com/oauth/authorize?response_type=code&scope=read_write&client_id=' + ConfigSrvc.stripeClientId + '&state=' + c.guid + '&stripe_user[business_name]=Neighborhood driver&stripe_user[business_type]=sole_prop&stripe_user[physical_product]=false&stripe_user[url]=http://www.fetch1.com/', '_system');
-
-        	//wait for confirmation
-        	ticker = $interval(function () {
-        		$http.get(ConfigSrvc.serviceUrl + '/api/auth?guid=' + c.guid)
+            //wait for confirmation
+            ticker = $interval(function () {
+                $http.get(ConfigSrvc.serviceUrl + '/api/auth?guid=' + c.guid)
 					.then(function (response) {
-						if (response.data !== "") {
-							$interval.cancel(ticker);
-							MemorySrvc.set('myId', response.data);
-							c.win.close();
-							page.load('driver/1_pickupMap/pickupMap.html');
-						}
+					    if (response.data !== "") {
+					        $interval.cancel(ticker);
+					        MemorySrvc.set('myId', response.data);
+					        c.win.close();
+					        page.load('driver/1_pickupMap/pickupMap.html');
+					    }
 					}, function (e) {
-						//This happens first try if the service is sleeping
-				});
-        	}, 1000);
+					    //This happens first try if the service is sleeping
+					});
+            }, 1000);
+
+            //atempt confirmation
+        	c.win = window.open('https://connect.stripe.com/oauth/authorize?response_type=code&scope=read_write&client_id=' + ConfigSrvc.stripeClientId + '&state=' + c.guid + '&stripe_user[business_name]=Neighborhood driver&stripe_user[business_type]=sole_prop&stripe_user[physical_product]=false&stripe_user[url]=http://www.fetch1.com/', '_blank');
         };
 
     	//check version
