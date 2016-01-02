4997d3ba62931578d4a0b03c73d9e1dd3a91b575
joeharker <AhtXtqgCbIKnJOstZwBN>
2016-01-02 14:24:44 -0700

    client update

--- Fetch1/app/config/configService.js
+++ Fetch1/app/config/configService.js
@@ -5,9 +5,15 @@ function () {
     var s = {};
     	
     s.version = '1.0.21';
-    s.stripeClientId = 'ca_5oSXm50hso4vFBv5IvDHVVklCiUkdP8R';
-    //s.stripeCheckoutKey = 'pk_test_LDonp15qE0gZigryIcfweU23';   //test
+
+    /*/
+    s.stripeClientId = 'ca_5oSXm50hso4vFBv5IvDHVVklCiUkdP8R';   //test
+    s.stripeCheckoutKey = 'pk_test_LDonp15qE0gZigryIcfweU23';   //test
+    //*/
+    s.stripeClientId = 'ca_5oSXcUIhx35b9UvKR7juLkb4pbzeDTgz';   //prod
     s.stripeCheckoutKey = 'pk_live_05MT9JT8ZWe1Q4BethIbziTv';   //prod
+    //*/
+
     s.serviceUrl = 'http://fetch001.azurewebsites.net';
     s.driverCut = 0.90;
 
