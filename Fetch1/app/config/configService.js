8525611f2d0f78bcd3d07512bb35d3d36117c1a9
joeharker <AhtXtqgCbIKnJOstZwBN>
2016-01-02 13:07:14 -0700

    live payment test

--- Fetch1/app/config/configService.js
+++ Fetch1/app/config/configService.js
@@ -4,9 +4,10 @@ function () {
     'use strict';
     var s = {};
     	
-    s.version = '1.0.20';
+    s.version = '1.0.21';
     s.stripeClientId = 'ca_5oSXm50hso4vFBv5IvDHVVklCiUkdP8R';
-    s.stripeCheckoutKey = 'pk_test_LDonp15qE0gZigryIcfweU23';
+    //s.stripeCheckoutKey = 'pk_test_LDonp15qE0gZigryIcfweU23';   //test
+    s.stripeCheckoutKey = 'pk_live_05MT9JT8ZWe1Q4BethIbziTv';   //prod
     s.serviceUrl = 'http://fetch001.azurewebsites.net';
     s.driverCut = 0.90;
 
