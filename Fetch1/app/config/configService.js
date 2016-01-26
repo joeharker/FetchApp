55eda82c5897fa40efb86bf1ad8758de320e1167
joeharker <AhtXtqgCbIKnJOstZwBN>
2016-01-25 19:03:39 -0700

    some changes to allow web use

--- Fetch1/app/config/configService.js
+++ Fetch1/app/config/configService.js
@@ -4,12 +4,12 @@ function () {
     'use strict';
     var s = {};
     	
-    s.version = '1.0.23';
+    s.version = '1.0.24';
 
-    /*/
+    //*/
     s.stripeClientId = 'ca_5oSXm50hso4vFBv5IvDHVVklCiUkdP8R';   //test
     s.stripeCheckoutKey = 'pk_test_LDonp15qE0gZigryIcfweU23';   //test
-    //*/
+    /*/
     s.stripeClientId = 'ca_5oSXcUIhx35b9UvKR7juLkb4pbzeDTgz';   //prod
     s.stripeCheckoutKey = 'pk_live_05MT9JT8ZWe1Q4BethIbziTv';   //prod
     //*/
