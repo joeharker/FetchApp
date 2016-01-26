e25b91fed8c46760ff3718e30f367d0134f670a0
joeharker <AhtXtqgCbIKnJOstZwBN>
2016-01-25 19:42:45 -0700

    1.0.24

--- Fetch1/app/config/configService.js
+++ Fetch1/app/config/configService.js
@@ -4,7 +4,7 @@ function () {
     'use strict';
     var s = {};
     	
-    s.version = '1.0.25';
+    s.version = '1.0.24';
 
     //*/
     s.stripeClientId = 'ca_5oSXm50hso4vFBv5IvDHVVklCiUkdP8R';   //test
